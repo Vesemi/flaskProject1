bbf24e70c67b25e68178717af77276f6489e3d93
vesemi <czyzewski.kamil@outlook.com>
2023-02-11 21:39:59 +0100

    added user last seen to database and user model

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
+
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
