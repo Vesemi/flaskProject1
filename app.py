d463f11ddea0ed109b475c68ef84dfc8cec357fd
vesemi <czyzewski.kamil@outlook.com>
2023-02-26 19:24:34 +0100

    general cleanup

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ login.login_view = 'login'
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("content.html")
 
@@ -31,9 +30,7 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+        next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
--- models.py
+++ models.py
@@ -9,10 +9,12 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    access = db.Column(db.Integer)
+    last_seen = db.Column(db.DateTime, default=None)
+# Relations
     tasks = db.relationship('Task', backref='contractor', foreign_keys="[Task.contractor_id]")
     tasks2 = db.relationship('Task', backref='creator', foreign_keys="[Task.creator_id]")
     comment = db.relationship('Comment', backref='creator', foreign_keys="[Comment.creator_id]")
-    last_seen = db.Column(db.DateTime, default=None)
 
     def __repr__(self):
         return f'{self.username}'
@@ -29,7 +31,7 @@ class Comment(db.Model):
     task = db.Column(db.Integer, db.ForeignKey('task.id'))
     text = db.Column(db.String(512))
     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    timestamp_created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp_created = db.Column(db.DateTime, index=True, default=datetime.utcnow().astimezone())
 
 
 class Task(db.Model):
