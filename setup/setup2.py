337cd7e089ac2e840cc3ac49ebc4c71ab0a71388
Vesemi <78653685+Vesemi@users.noreply.github.com>
2023-03-12 23:11:34 +0100

    Update setup2.py
    updated

--- setup/setup2.py
+++ setup/setup2.py
@@ -1,10 +1,9 @@
-import configparser
 import os
 import sys
 
 
 def create_nginx_and_gunicorn_files(domena, projekt):
-    # Edytujemy plik nginx:
+    # Wczytanie plików nginx oraz gunicorn
     plik_nginxa = ''
     plik_nginxa_template = open('/var/www/' + projekt + '/setup/nginx_file').readlines()
 
@@ -16,22 +15,22 @@ def create_nginx_and_gunicorn_files(domena, projekt):
         l = l.replace('NAZWA_PROJEKTU', projekt)
         plik_nginxa += l
 
-    # Zapisujemy plik nginx:
-    with open("/etc/nginx/sites-available/{}".format(domena), "w") as file1:
-        file1.write(plik_nginxa)
+    # Zapis edytowanego szablonu nginx:
+    with open(f"/etc/nginx/sites-available/{domena}", "w") as plik1:
+        plik1.write(plik_nginxa)
 
-    # Robimy link do kopii pliku nginx:
-    os.system('sudo ln -s /etc/nginx/sites-available/{} /etc/nginx/sites-enabled/'.format(domena))
+    # Stworzenie linku do kopii pliku nginx:
+    os.system(f'sudo ln -s /etc/nginx/sites-available/{domena} /etc/nginx/sites-enabled/')
 
     for l in plik_gunicorn_template:
         l = l.replace('NAZWA_PROJEKTU', projekt)
         plik_gunicorn +=l
 
-    # Zapisujemy plik gunicorn
-    with open("/etc/systemd/system/" + projekt + '.service', "w") as file2:
-        file2.write(plik_gunicorn)
+    # Zapis edytowanego szablonu pliku gunicorn
+    with open("/etc/systemd/system/" + projekt + '.service', "w") as plik2:
+        plik2.write(plik_gunicorn)
 
-    # Restartujemy startujemy serwis flagi, restartujemy nginxa.
+    # Restart serwisu,oraz restart WSGI Nginx.
     os.system('sudo systemctl start ' + projekt + '.service')
     os.system('sudo systemctl daemon-reload')
     os.system('sudo systemctl restart nginx')
@@ -42,5 +41,3 @@ if __name__ == '__main__':
         domena = sys.argv[1]
         projekt = sys.argv[2]
         create_nginx_and_gunicorn_files(domena, projekt)
-        print('Twoja domena to:', domena)
-        print('Przygotowuje pliki serwerowe.')
