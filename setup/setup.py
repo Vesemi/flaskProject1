78af090453847ec34148ca62bb74e035df6e6237
Vesemi <78653685+Vesemi@users.noreply.github.com>
2023-03-13 10:28:29 +0100

    Update setup.py
    gfd

--- setup/setup.py
+++ setup/setup.py
@@ -1,7 +1,8 @@
 import os
 
-#Instalacja podstawowych pakietów do uruchomienia aplickacji napisanych w pythconie
-os.system('sudo apt install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools --yes')
+#Instalacja podstawowych pakietów do uruchomienia aplikacji napisanych w pythconie
+os.system('sudo apt install python3-pip python3-dev python3-venv build-essential\
+          libssl-dev libffi-dev python3-setuptools --yes')
 
 # Deinstalacja apache2 oraz instalacja nginx
 os.system('sudo apt remove apache2 --yes')
